b4665237f2ced2890d9119f260d5278611ac70bc
yafet <yafetesfaye11@gmail.com>
2025-07-20 13:21:19 +0300

    feat: add search product page

--- onboarding/e_commerce/lib/app.dart
+++ onboarding/e_commerce/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/screens/add_update_page.dart';
 import 'package:e_commerce/screens/home_page.dart';
+import 'package:e_commerce/screens/search_product_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AddUpdatePage(),
+      home: SearchProductPage(),
     );
   }
 }
---
+++ onboarding/e_commerce/lib/screens/search_product_page.dart
@@ -0,0 +1,151 @@
+import 'package:e_commerce/data.dart';
+import 'package:flutter/material.dart';
+
+class SearchProductPage extends StatefulWidget {
+  const SearchProductPage({super.key});
+
+  @override
+  State<SearchProductPage> createState() => _SearchProductPageState();
+}
+
+class _SearchProductPageState extends State<SearchProductPage> {
+  double _sliderValue = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 252, 251, 251),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: Icon(
+          Icons.arrow_back_ios_new,
+          color: const Color.fromARGB(255, 38, 38, 231),
+          size: 14,
+        ),
+        backgroundColor: const Color.fromARGB(255, 252, 251, 251),
+        title: Center(
+          child: Text('Search Product', style: TextStyle(fontSize: 14)),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 10,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      hintText: 'Leather',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      suffixIcon: Icon(
+                        Icons.arrow_right_alt,
+                        size: 24,
+                        color: Color.fromARGB(255, 38, 38, 231),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 38, 38, 231),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Icon(
+                    Icons.format_align_center_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 3,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: ListView(children: data),
+            ),
+          ),
+
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(color: Colors.white),
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Category'),
+                  SizedBox(height: 7),
+                  TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 10),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(
+                          color: Colors.grey.withAlpha(90),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text('Price'),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Color.fromARGB(255, 38, 38, 231),
+                      inactiveTrackColor: Colors.indigo[100],
+                      thumbColor: Colors.white,
+                      overlayColor: Colors.indigo.withOpacity(0.2),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10),
+                      trackHeight: 6,
+                    ),
+                    child: Slider(
+                      value: _sliderValue,
+                      min: 0,
+                      max: 100,
+                      divisions: 100,
+                      label: _sliderValue.round().toString(),
+                      onChanged: (value) {
+                        setState(() {
+                          _sliderValue = value;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 38, 38, 231),
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'APPLY',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
